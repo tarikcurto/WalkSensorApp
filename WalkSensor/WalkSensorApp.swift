99510045e2d9686f0a8667022c311b1ce31d8596
tarikcurto <centro.tarik@live.com>
2023-11-11 15:45:18 +0100

    Initial Commit

---
+++ WalkSensor/WalkSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkSensorApp.swift
+//  WalkSensor
+//
+//  Created by Tarik Curto on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WalkSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
